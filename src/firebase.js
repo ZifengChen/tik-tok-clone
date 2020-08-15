eb416859d5e6462e6fac40dba64f9fcbf1bb555c
ZifengChen <chen1677@purdue.edu>
2020-08-15 18:01:03 +0800

    first commit

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyB5x_VZk5_nqxQRdC4iOMjgOtvBIxxl4DM",
+  authDomain: "tik-tok-clone36.firebaseapp.com",
+  databaseURL: "https://tik-tok-clone36.firebaseio.com",
+  projectId: "tik-tok-clone36",
+  storageBucket: "tik-tok-clone36.appspot.com",
+  messagingSenderId: "832328729497",
+  appId: "1:832328729497:web:19cbce15251582b5be0908",
+  measurementId: "G-9JL73L9DW4"
+}
